9e1cfee78343568669c2f7293dbe41741b7fa6a1
raineryue <it_jinlei.yue@163.com>
2016-03-31 14:08:58 +0800

    CALayer知识点梳理：创建一个自定义layer来显示图片
    CALayer知识点梳理：创建一个自定义layer来显示图片

--- 20160331002-CALayer-CreateLayer/ViewController.m
+++ 20160331002-CALayer-CreateLayer/ViewController.m
@@ -16,7 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 1.创建一个CALayer层
+    CALayer *layer = [[CALayer alloc] init];
+    
+    // 2.设置layer的宽高位置
+    layer.frame = CGRectMake(50, 50, 200, 200);
+    
+    // 3.设置layer的内容
+    layer.contents = (id)[UIImage imageNamed:@"阿狸头像"].CGImage;
+    
+    // 4.讲自定义layer添加到图片视图的layer层上
+    [self.view.layer addSublayer:layer];
 }
 
 - (void)didReceiveMemoryWarning {
